931d92421ab02805f7d508bb6def89936f73ec6f
moondiverc <nezzaluna10@gmail.com>
2025-07-31 21:07:12 +0700

    fix: styling

---
+++ src/app/(menu)/features/page.tsx
@@ -0,0 +1,8 @@
+export default function FeaturesPage() {
+  return (
+    <div>
+      <h1>Features</h1>
+      <p>Explore the powerful features designed to accelerate your career.</p>
+    </div>
+  );
+}
