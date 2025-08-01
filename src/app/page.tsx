24be976d7aceb0fbbd8f8e2e6341c3609e176d7a
moondiverc <nezzaluna10@gmail.com>
2025-08-01 23:10:20 +0700

    fix: background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -76,20 +76,13 @@ export default function Home() {
           </p>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             <div className="bg-white/20 backdrop-blur-sm border border-white/30 p-8 rounded-3xl text-center hover:bg-white/30 transition duration-200">
-              <div className="w-16 h-16 bg-blue-500 rounded-full flex items-center justify-center mx-auto mb-6">
-                <svg
-                  className="w-8 h-8 text-white"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                  />
-                </svg>
+              <div className="w-16 h-16 flex items-center justify-center mx-auto mb-6">
+                <Image
+                  src="/element1.png"
+                  alt="Map Icon"
+                  width={100}
+                  height={100}
+                />
               </div>
               <h3 className="font-bold text-black text-xl mb-4">
                 Personalize Career Map
@@ -101,20 +94,13 @@ export default function Home() {
               </p>
             </div>
             <div className="bg-white/20 backdrop-blur-sm border border-white/30 p-8 rounded-3xl text-center hover:bg-white/30 transition duration-200">
-              <div className="w-16 h-16 bg-purple-500 rounded-full flex items-center justify-center mx-auto mb-6">
-                <svg
-                  className="w-8 h-8 text-white"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-                  />
-                </svg>
+              <div className="w-16 h-16 flex items-center justify-center mx-auto mb-6">
+                <Image
+                  src="/element2.png"
+                  alt="Map Icon"
+                  width={100}
+                  height={100}
+                />
               </div>
               <h3 className="font-bold text-black text-xl mb-4">
                 CV Analyzer & Feedback
@@ -125,20 +111,13 @@ export default function Home() {
               </p>
             </div>
             <div className="bg-white/20 backdrop-blur-sm border border-white/30 p-8 rounded-3xl text-center hover:bg-white/30 transition duration-200">
-              <div className="w-16 h-16 bg-green-500 rounded-full flex items-center justify-center mx-auto mb-6">
-                <svg
-                  className="w-8 h-8 text-white"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.746 0 3.332.477 4.5 1.253v13C19.832 18.477 18.246 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
-                  />
-                </svg>
+              <div className="w-16 h-16 flex items-center justify-center mx-auto mb-6">
+                <Image
+                  src="/element3.png"
+                  alt="Map Icon"
+                  width={100}
+                  height={100}
+                />
               </div>
               <h3 className="font-bold text-black text-xl mb-4">
                 Training & Certification
